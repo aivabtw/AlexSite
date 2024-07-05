7f5b2963814f188b1528851d4967395d9bfd0580
1CodePhantom1 <116742799+1CodePhantom1@users.noreply.github.com>
2024-07-05 18:24:53 +0300

    Delted useless files

--- main.py
+++
@@ -1,8 +0,0 @@
-import discord
-from discord.ext import commands, tasks
-import mains
-import os
-
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix="/")
-mains.keep_alive()
-bot.run(os.environ.get('bots'))
